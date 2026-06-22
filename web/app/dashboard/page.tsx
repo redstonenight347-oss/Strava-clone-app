b077034cef371d3081b5ded7ca9bd92c78ac59e1
redstonenight347-oss <redstonenight347@gmail.com>
2026-06-22 16:46:05 +0530

    add dashboard prototype

--- web/app/dashboard/page.tsx
+++ web/app/dashboard/page.tsx
@@ -1,24 +1,39 @@
 import { getActivitiesByUser } from "@repo/db"
+import ActivityCard from "@/components/ActivityCard"
 
-export default async function dashboard() {
-//TODO: should pass the jwt rather than raw number
-  const activities = await getActivitiesByUser(1); 
+//TODO: create a root types then import it from there
+export type OverviewType = {
+  id: number,
+  name: string,
+  value: number
+}
+
+
+export default async function Dashboard() {
+  //TODO: should pass the jwt rather than raw number
+  // const activities = await getActivitiesByUser(1); 
+
+  //the values will come from the db and change the object layout
+  const overview: OverviewType[] = [
+    { 
+      id: 0,
+      name: "Distance", 
+      value: 77.7 //in meters
+    },{ 
+      id: 1,
+      name: "Elev Gain", 
+      value: 10 //in meters
+    },{ 
+      id: 2,
+      name: "Time", 
+      value: 25 //in seconds
+    }  
+  ]
 
   return (
     <div>
-      <h1>My Activities</h1>
-
-      {activities.length === 0 && <p>No activities yet</p>}
-
-      {activities.map((activity) => (
-        <div key={activity.activityId}>
-          <h2>Title: {activity.title ?? "Untitled"}</h2>
-          <p>{activity?.description}</p>
-          <p>Type: {activity.type}</p>
-          <p>Distance: {activity.distance}Km</p>
-          <p>Duration: {activity.duration}mins</p>
-        </div>
-      ))}
+      <ActivityCard overview={overview} />
     </div>
   )
 }
+
---
+++ web/components/ActivityCard.tsx
@@ -0,0 +1,57 @@
+
+import Image from "next/image"
+import { OverviewType } from "@/app/dashboard/page"
+
+type ActivityCardType = {
+  overview: OverviewType[]
+}
+
+export default function ActivityCard({overview}: ActivityCardType) {
+
+  return (
+    <div className="mt-8 p-4 m-auto w-[90%] md:w-[60%] lg:w-[35%] bg-amber-200 rounded-md">
+      <div className="flex justify-start">
+        <Image
+          width={48}
+          height={48}
+          src="/temphoto.png"
+          alt="Profile"
+          className="mr-3 mt-3 w-12 rounded-full" />
+        <div className="mt-3 h-0 flex flex-col justify-start">
+          <h3 className="mb-0 text-lg/tight font-semibold">Username</h3>
+          <p className="mt-0 text-sm/tight">date, time and device</p>
+          <p className="mt-0 text-sm/tight">location</p>
+        </div>
+      </div>
+
+
+      <h1 className="mt-4 text-2xl font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+      <div className="mt-2 flex gap-8">
+        {overview.map((o) => {
+          return (
+            <div key={o.id}> 
+              <p>{o.name}</p>
+              <h2 className="text-2xl font-semibold">{o.value}</h2>
+            </div>
+          )
+        })}
+      </div>
+
+
+      <div className="mt-4 w-full h-80 bg-amber-50">
+        {/* map image */}
+      </div>
+
+
+      <div className="mt-8 mb-4 flex justify-around md:justify-start gap-12">
+        <svg fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" width="24" height="24" data-testid="unfilled_kudos"><path d="M6.18.36A.625.625 0 016.746 0h.366a2.625 2.625 0 012.609 2.918L9.374 6h3.69a2.185 2.185 0 011.68 3.584l-.119.142v1.291c0 .458-.16.902-.454 1.254l-.171.205v.399A2.125 2.125 0 0111.875 15H5.703c-.256 0-.507-.077-.72-.22l-1.157-.777a.042.042 0 00-.024-.007l-1.483.031A1.292 1.292 0 011 12.736V8.81c0-.38.168-.742.46-.988l2.032-1.711zm.964.89L4.566 6.765a.625.625 0 01-.163.213l-2.138 1.8a.042.042 0 00-.015.032v3.926c0 .023.02.042.043.041l1.483-.03c.266-.006.527.07.748.219l1.156.777a.042.042 0 00.023.007h6.172a.875.875 0 00.875-.875v-.851l.46-.553a.708.708 0 00.165-.454V9.274l.408-.49a.935.935 0 00-.718-1.534h-5.09l.504-4.471c.09-.805-.53-1.51-1.335-1.529z" fill=""></path></svg>
+        <svg fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" width="24" height="24"><g fill=""><path d="M3 5.75h10V7H3zM3 8v1.25h7V8z"></path><path d="M0 3.958C0 2.877.877 2 1.958 2h12.084C15.123 2 16 2.877 16 3.958v7.084A1.958 1.958 0 0114.042 13H7.759l-2.636 2.636A1.243 1.243 0 013 14.756V13H1.958A1.958 1.958 0 010 11.042zm1.958-.708a.708.708 0 00-.708.708v7.084c0 .39.317.708.708.708H4.25v2.991l2.991-2.991h6.8a.708.708 0 00.709-.708V3.958a.708.708 0 00-.708-.708z"></path></g></svg>
+        <svg fill="#000000" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="#000000" strokeWidth="0.00024">
+          <g strokeWidth="0" />
+          <g strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.528" />
+          <path d="M1160,104a3.986,3.986,0,0,1-3.19-1.6l-8.84,5.292c0.01,0.105.03,0.2,0.03,0.312,0,0.128-.03.249-0.04,0.374l8.8,5.3A3.986,3.986,0,1,1,1156,116c0-.149.03-0.291,0.04-0.436l-8.82-5.216a4,4,0,1,1-.06-4.777l8.88-5.189c-0.01-.127-0.04-0.251-0.04-0.382A4,4,0,1,1,1160,104Zm0,14a2,2,0,1,0-2-2A2,2,0,0,0,1160,118Zm-16-12a2,2,0,1,0,2,2A2,2,0,0,0,1144,106Zm16-8a2,2,0,1,0,2,2A2,2,0,0,0,1160,98Z" transform="translate(-1140 -96)" />
+        </svg>
+      </div>
+    </div>
+  )
+}
